c714e409d5fc810f51806ba0e2dff23575d3763a
Derek P <>
2018-04-24 14:22:51 -0500

    finish data structure, started dfs func, pushing to stack

--- dfs.c
+++ dfs.c
@@ -6,36 +6,59 @@ struct vertex{
 
   char label;
   int visit;
+  int edges;
+  //struct vertex **e = malloc(sizeof(struct vertex*)*5);
   struct vertex *e1,*e2,*e3,*e4,*e5;
 
 };
 
 void visit(struct vertex *cursor);
-struct vertex **push(struct vertex *ins,int count);
+void push(struct vertex *ins,int count,struct vertex** stack);
 struct vertex **pop(struct vertex *ins,int count);
 struct vertex *populate();
-void dfs(struct vertex *head);
+void dfs(struct vertex *head, struct vertex **stack, int stackcount);
 
 int main(int argc, char *argv[]){
 
   struct vertex *head = populate();
+  struct vertex **stack = malloc (sizeof(struct vertex *)*12);
+  int stackcount = 0;
+  dfs(head,stack,stackcount);
   // vA.label = 'a';
   //vA.visit = 0;
-  visit(head);
+  /* visit(head); */
+  /* visit(head->e1); */
+  /* visit(head->e2); */
+  /* visit(head->e3); */
+  /* if(!head->e4){ */
+  /*   printf("head e4 does not exist\n\n"); */
+  /* } */
   //printf("%d\n\n",vA.visit);
 
 }
 
-void dfs(struct vertex *head){
+void dfs(struct vertex *cursor,struct vertex **stack, int stackcount){
 
-  struct vertex **stack;
-  int stackcount;
+
+  /* struct vertex *cursor = head; */
+  visit(cursor);
+  if(cursor->edges>1){
+    push(cursor,stackcount,stack);
+    stackcount++;
+    // printf("pushing %c\nstack at %d\n",cursor->label,stackcount-1);
+    //visit(stack[stackcount-1]);
+  }
+  if((cursor->e1)&&(cursor->e1->visit==0)){
+    dfs(cursor->e1,stack,stackcount);
+  }
+  
 
 }
 
-struct vertex** push(struct vertex *ins,int count){//push vertex pointers into stack for backtracing
+void push(struct vertex *ins,int count,struct vertex** stack){//push vertex pointers into stack for backtracing
 
-
+  stack[count]=ins;
+  //return stack;
 
 }
 
@@ -48,6 +71,7 @@ struct vertex ** pop(struct vertex *del,int count){//after deadend is found pop 
 void visit(struct vertex *cursor){
 
   cursor->visit = 1;
+  cursor->edges--;
   printf("%c \n",cursor->label);
 
 }
@@ -70,23 +94,23 @@ struct vertex *populate(){//fills the list with the required vertices
   struct vertex *vn = malloc(sizeof(struct vertex));
   struct vertex *vo = malloc(sizeof(struct vertex));
   struct vertex *vp = malloc(sizeof(struct vertex));
-  va->label='a',va->e1 = vb,va->e2 = vf,va->e3=ve;
-  vb->label='b',vb->e1 = va,vb->e2 = vf,vb->e3=vc;
-  vc->label='c',vc->e1 = vb,vc->e2 = vg,vc->e3=vd;
-  vd->label='d',vd->e1 = vc,vd->e2 = vg,vd->e3=vh;
-  ve->label='e',ve->e1 = va,ve->e2 = vf,ve->e3=vi;
-  vf->label='f',vf->e1 = va,vf->e2 = vb,vf->e3=ve,vf->e4  = vi;
-  vg->label='g',vg->e1 = vc,vg->e2 = vd,vg->e3=vl,vg->e4=vk,vg->e5=vj;
-  vh->label='h',vh->e1 = vd,vh->e2 = vl;
-  vi->label='i',vi->e1  = ve ,vi->e2  = vf ,vi->e3  = vj;//
-  vj->label='j',vj->e1  = v ,vj->e2  = v ,vj->e3  = v;
-  vk->label='k',vk->e1  = v ,vk->e2  = v ,vk->e3  = v;
-  vl->label='l',vl->e1  = v ,vl->e2  = v ,vl->e3  = v;
-  vm->label='m',vm->e1  = v ,vm->e2  = v ,vm->e3  = v;
-  vn->label='n',vn->e1  = v ,vn->e2  = v ,vn->e3  = v;
-  vo->label='o',vo->e1  = v ,vo->e2  = v ,vo->e3  = v;
-  vp->label='p',vp->e1  = v ,vp->e2  = v ,vp->e3  = v;
-
+  va->label='a',va->e1 = ve,va->e2 = vf,va->e3=vb,va->edges=3;
+  vb->label='b',vb->e1 = va,vb->e2 = vf,vb->e3=vc,vb->edges=3;
+  vc->label='c',vc->e1 = vb,vc->e2 = vg,vc->e3=vd,vc ->edges=3;
+  vd->label='d',vd->e1 = vc,vd->e2 = vg,vd->e3=vh,vd ->edges=3;
+  ve->label='e',ve->e1 = vi,ve->e2 = vf,ve->e3=va,ve ->edges=3;
+  vf->label='f',vf->e1 = va,vf->e2 = ve,vf->e3=vi,vf->e4  = vb,vf ->edges=4;
+  vg->label='g',vg->e1 = vj,vg->e2 = vk,vg->e3=vl,vg->e4=vd,vg->e5=vc,vg ->edges=5;
+  vh->label='h',vh->e1 = vd,vh->e2 = vl,vh ->edges=2;
+  vi->label='i',vi->e1 = vm ,vi->e2 = vn ,vi->e3 = vj,vi->e4 = vf,vi->e5 = ve,vi ->edges=5;
+  vj->label='j',vj->e1 = vi ,vj->e2 = vk ,vj->e3 = vg,vj ->edges=3;  
+  vk->label='k',vk->e1 = vo ,vk->e2 = vj ,vk->e3 = vg,vk ->edges=3;
+  vl->label='l',vl->e1 = vh ,vl->e2 = vg ,vl->e3 = vp,vl ->edges=3;
+  vm->label='m',vm->e1 = vn ,vm->e2 = vi,vm->edges=2;
+  vn->label='n',vn->e1 = vk ,vn->e2 = vi ,vn->e3 = vm,vn->edges=3;
+  vo->label='o',vo->e1 = vp ,vo->e2 = vk,vo->edges=2;
+  vp->label='p',vp->e1 = vl ,vp->e2 = vo,vp->edges=2;
+ 
   return va;
   
 }
